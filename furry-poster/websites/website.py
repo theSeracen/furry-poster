24200093ac5af75d73b4de886e568d57d20c4890
Seracen <neededn@gmail.com>
2020-02-08 14:55:12 +1000

    Change to tab formatting

--- furry-poster/websites/website.py
+++ furry-poster/websites/website.py
@@ -7,21 +7,21 @@ class WebsiteError(Exception): pass
 class AuthenticationError(WebsiteError):pass
 
 class Website(ABC):
-    def __init__(self, name, csvTags=False):
-        self.name = name
-        self.csvTags = csvTags
+	def __init__(self, name, csvTags=False):
+		self.name = name
+		self.csvTags = csvTags
 
-    @abstractmethod
-    def submitStory(self, title, description, tags, story, thumbnail):
-        """Send story and submit it via website mechanisms"""
-        pass
+	@abstractmethod
+	def submitStory(self, title, description, tags, story, thumbnail):
+		"""Send story and submit it via website mechanisms"""
+		pass
 
-    @abstractmethod
-    def testAuthentication(self):
-        """Test that the user is properly authenticated on the site"""
-        pass
+	@abstractmethod
+	def testAuthentication(self):
+		"""Test that the user is properly authenticated on the site"""
+		pass
 
-    @abstractmethod
-    def validateTags(self):
-        """Convert the given tag string to a form that is valid on the site"""
-        pass
+	@abstractmethod
+	def validateTags(self):
+		"""Convert the given tag string to a form that is valid on the site"""
+		pass
