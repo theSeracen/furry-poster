1900769caa46f05e249071752a59c6700bbfa467
Seracen <neededn@gmail.com>
2020-02-13 16:37:15 +1000

    Add stage markers

--- commandmain.py
+++ commandmain.py
@@ -48,7 +48,7 @@ def initSite(regexString: str, site: Website, ignore_errors: bool) -> Optional[W
 		if ignore_errors is False:
 			raise AuthenticationError('{} cannot find a cookies file'.format(site.name))
 		else:
-			print('{} cannot find cookies; the site will be skipped'.format(site.name))
+			print('{}{} cannot find cookies; the site will be skipped'.format('[Site Init]\t', site.name))
 	else:
 		cj = http.cookiejar.MozillaCookieJar(cookiesLoc)
 		cj.load()
@@ -61,7 +61,7 @@ def initSite(regexString: str, site: Website, ignore_errors: bool) -> Optional[W
 			else:
 				raise
 		else:
-			print('{} successfully authenticated'.format(site.name))
+			print('{}{} successfully authenticated'.format('[Site Init]\t', site.name))
 			return site
 	return None
 
@@ -82,6 +82,8 @@ def main():
 	
 	sites = filter(None, sites)
 
+	stage = '[Loading]\t'
+
 	#now we can go into checking for all the required items
 	if os.path.isdir(args.directory) is False: raise Exception("Valid directory required")
 
@@ -90,7 +92,7 @@ def main():
 	if args.tags is None: args.tags = input('Please enter CSV tags: ')
 
 	args.description = args.description.replace('\\n', '\n') #replaces newline from manual input as it doesn't add an escape
-
+	
 	#error checking
 	if args.title == '': raise Exception('No title specified!')
 	if args.description == '': raise Exception('No description specified!')
@@ -98,12 +100,12 @@ def main():
 
 	if args.post_script:
 		if os.path.exists('post-script.txt'):
-			print('Post-script found')
+			print('{}Post-script found'.format(stage))
 			with open('post-script.txt','r',encoding='utf-8') as post:
 				args.description = args.description + '\n\n' + ''.join(post.readlines())
 		else:
 			if args.ignore_errors:
-				print('Post-script file cannot be loaded!\nContinuing...')
+				print('{}Post-script file cannot be loaded!\nContinuing...'.format(stage))
 			else:
 				raise Exception('Post-script file cannot be found')
 	
@@ -120,13 +122,13 @@ def main():
 	for file, ending in ((file, ending) for ending in ends for file in os.listdir(args.directory)):
 		if file.endswith(ending): 
 			storyLoc = args.directory + '\\' + file
-			print('File found: {}'.format(storyLoc))
+			print('{}File found: {}'.format(stage, storyLoc))
 			break
 	if storyLoc is None: raise Exception('No story file of format {} found!'.format(args.format))
 
 	if args.check_source:
 		#load story and run checks
-		print('Checking source file...')
+		print('{}Checking source file...'.format(stage))
 		with open(storyLoc, 'r', encoding='utf-8') as file:
 			checkStory =  file.read()
 		if args.format == 'markdown':
@@ -138,24 +140,24 @@ def main():
 		else:
 			raise TypeError('Cannot format format type'.format(args.format))
 		if newcheck != checkStory:
-			print('Writing source changes...')
+			print('{}Writing source changes...'.format(stage))
 			with open(storyLoc, 'w', encoding='utf-8') as file:
 				file.write(newcheck)
-		else: print('No changes to source needed')
+		else: print('{}No changes to source needed'.format(stage))
 
 	#get thumbnail
 	if args.generate_thumbnail:
 		splitTags = args.tags.split(', ')
-		print('Creating thumbnail...')
+		print('{}Creating thumbnail...'.format(stage))
 		try:
 			thumbnailPass = thumbnailgeneration.makeThumbnail(args.title, splitTags, args.generate_thumbnail)
 			if args.messy:
-				print('Saving thumbnail to file...')
+				print('{}Saving thumbnail to file...'.format(stage))
 				with open(args.directory + '\\thumbnail.png', 'wb') as file:
 					file.write(thumbnailPass.getvalue())
 		except thumbnailerrors.ThumbnailSizingError:
 			if args.ignore_errors:
-				print('Thumbnail generation has failed!')
+				print('{}Thumbnail generation has failed!'.format(stage))
 				thumbnailPass = None
 			else:
 				raise
@@ -165,21 +167,22 @@ def main():
 			for file in os.listdir(args.directory):
 				if re.match('.*\\.(png|jpg)', file):
 					thumbnailLoc = args.directory + '\\' + file
-					print('Thumbnail file found')
+					print('{}Thumbnail file found'.format(stage))
 					break
 			if thumbnailLoc is None:
 				if args.ignore_errors:
-					print('No thumbnail found!\nContinuing...')
+					print('{}No thumbnail found!\nContinuing...'.format(stage))
 				else:
 					raise Exception('No thumbnail file found!')
 		if thumbnailLoc is None: thumbnailPass = None
 		else: thumbnailPass = open(thumbnailLoc, 'rb')
 
+	stage = '[Processing]\t'
 	#submit the files to each website
 	for site in sites:
 		#convert the description if necessary
 		if site.preferredFormat == 'bbcode':
-			print('Converting description to bbcode...')
+			print('{}Converting description to bbcode...'.format(stage))
 			description = markdownformatter.parseStringBBcode(args.description)
 		else: description = args.description
 		#handle the story files
@@ -190,22 +193,22 @@ def main():
 			#determine the type and convert
 			if args.format == 'bbcode':
 				if site.preferredFormat == 'markdown':
-					print('Converting story to markdown...')
+					print('{}Converting story to markdown...'.format(stage))
 					story = bbcodeformatter.parseStringMarkdown(loadedStory)
 				else:
 					raise Exception('Cannot convert BBcode to the format {}'.format(site.preferredFormat))
 			elif args.format == 'markdown':
 				if site.preferredFormat == 'bbcode':
-					print('Converting story to bbcode...')
+					print('{}Converting story to bbcode...'.format(stage))
 					story = markdownformatter.parseStringBBcode(loadedStory)
 				else:
 					raise Exception('Cannot convert markdown to the format'.format(site.preferredFormat))
 			elif args.format == 'html':
 				if site.preferredFormat == 'bbcode':
-					print('Converting story to bbcode...')
+					print('{}Converting story to bbcode...'.format(stage))
 					story = ''.join(htmlformatter.formatFileBBcode(StringIO(loadedStory)))
 				elif site.preferredFormat == 'markdown':
-					print('Converting story to markdown...')
+					print('{}Converting story to markdown...'.format(stage))
 					story = ''.join(htmlformatter.formatFileMarkdown(StringIO(loadedStory)))
 				else:
 					raise Exception('Cannot convert HTML to the format'.format(site.preferredFormat))
@@ -220,13 +223,14 @@ def main():
 					with open(''.join(storyLoc.split('.')[:-1]) + '.md', 'w', encoding='utf-8') as file:
 						file.write(story.getvalue())
 		try:
-			print('Beginning {} submission'.format(site.name))
+			stage = '[Posting]\t'
+			print('{}Beginning {} submission'.format(stage, site.name))
 			#reset virtual file stream if needed
 			thumbnailPass.seek(0)
 			story.seek(0)
-			if args.test: print('test: {} bypassed'.format(site.name))
+			if args.test: print('{}test: {} bypassed'.format(stage, site.name))
 			else: site.submitStory(args.title, description, args.tags, args.rating, story, thumbnailPass)
-			print('{} submission completed successfully'.format(site.name))
+			print('{}{} submission completed successfully'.format(stage, site.name))
 		except WebsiteError as e:
 			if args.ignore_errors: print('{} has failed with exception {}'.format(site.name, e))
 			else: raise
