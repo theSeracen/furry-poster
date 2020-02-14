f45ab66dcb0deba94f2926eb9b0b26cbb053ddac
Seracen <neededn@gmail.com>
2020-02-14 16:23:38 +1000

    Added cookies regex property to make all the code cleaner

--- furryposter/websites/sofurry.py
+++ furryposter/websites/sofurry.py
@@ -13,6 +13,7 @@ from typing import TextIO, BinaryIO, List, Dict
 class SoFurry(Website):
 	def __init__(self):
 		Website.__init__(self, 'sofurry', {'general':0, 'adult':1}, 'bbcode')
+		self.cookiesRegex = r'^(sofurry|sf)?(cookies?)?\.txt'
 
 	def submitStory(self, title: str, description: str, tags: str, passedRating: str, story: TextIO, thumbnail):
 		"""Send story and submit it via POST"""
--- transfer.py
+++ transfer.py
@@ -1,20 +1,61 @@
 import argparse
 import os
 from furryposter.story import Story
+from furryposter.websites import furaffinity, sofurry, weasyl, website
+import builtins
+
+stage = '[Init]\t'
+def print(inp: str): builtins.print(stage + inp)
+def setstage(newstage:str):
+	global stage
+	stage = newstage
 
 parser = argparse.ArgumentParser(prog="furrytransfer", description="Transfer galleries between furry sites")
 
+
 def initParser():
-	parser.add_argument('destination', metavar='D', help='site to transfer to')
-	parser.add_argument('source', metavar='S', help='site with gallery')
+	parser.add_argument('source', metavar='S', choice=['furaffinity','sofurry','weasyl'], help='site with gallery')
+	parser.add_argument('destination', metavar='D', choice=['furaffinity','sofurry','weasyl'], help='site to transfer to')
+	parser.add_argument('name', metavar='N', help='the name of the user to copy the gallery from')
 
 	parser.add_argument('-d', '--delay', type=int, default=5, help='seconds between posts to new site')
+	parser.add_argument('-t', '--thumbnail-behaviour', choice=['new', 'source', 'none'], default='new')
 
 def main():
 	initParser()
 	args = parser.parse_args()
+	if args.source == args.destination:
+		raise Exception('Source and destination sites cannot be the same')
+	if args.source == 'sofurry' and args.thumbnail_behaviour == 'source':
+		print('Default thumbnails are generated in all Sofurry submissions and these cannot be distinguished from uploaded thumbnails. Do you wish to continue with this in mind?')
+		response = input('(Yes/No')
+		if 'n' in response.lower():
+			print('Exiting...')
+			exit(1)
+	
+	source = determineSite(args.source)
+	dest = determineSite(args.destination)
+	setstage('[Scanning]\t')
+
+	source.crawlGallery()
 
 
+def determineSite(site: str) -> Website:
+	if site == 'furaffinity': 
+		site = furaffinity.FurAffinity()
+		site.load(__findCookies(site.cookiesRegex))
+	elif site == 'sofurry': 
+		site = sofurry.SoFurry()
+		site.load(__findCookies(site.cookiesRegex))
+	elif site == 'weasyl': 
+		site = weasyl.Weasyl()
+		site.load(__findCookies(site.cookiesRegex))
+	else: raise Exception("Website '{}' unrecognised".format(site))
 
+def __findCookies(regex: str) -> str:
+	for file in os.listdir(os.getcwd()):
+		if re.match(regex, file):
+			cookiesLoc = file
+			break
 if __name__ == '__main__':
 	main()
