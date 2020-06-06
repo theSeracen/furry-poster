71b418c49cbf2081ccacbb1c2f59c1d0fce7c7cb
Seracen <neededn@gmail.com>
2020-06-06 18:39:06 +1000

    Fix bug where concatenated files could be out of order

--- furryposter/utilities/fileconcat.py
+++ furryposter/utilities/fileconcat.py
@@ -5,7 +5,7 @@ from typing import List
 def concatFiles(file_list: List[str]):
     '''Load and concatenate the passed files'''
     file_content = []
-    for file_loc in file_list:
+    for file_loc in sorted(file_list):
         with open(file_loc, 'r') as file:
             file_content.append(file.read())
-    return '\n********\n'.join(file_content)
+    return '\n-------------\n'.join(file_content)
