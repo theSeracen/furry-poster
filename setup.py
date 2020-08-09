e604b6e0744bf1f204a9aabf876dee49dfd72a44
Seracen <neededn@gmail.com>
2020-08-09 13:49:48 +1000

    Update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup
 
-setup(
-    name='furryposter',
-    version='0.2',
-    description='Tool for distributing stories to several different furry websites from the command-line',
-    author='Seracen',
-    packages=['furryposter'],
-    install_requires=[
-        'requests',
-        'bs4',
-         'tqdm'])
+import setuptools
+
+setuptools.setup(install_requires=['pbr'], pbr=True)
